922a06e0b1f44755545cb96d2aa76ec4d0198169
2CTO <u201521691@upc.edu.pe>
2022-09-18 18:09:00 -0500

    first things to init working

---
+++ src/main/java/com/upc/pe/backenderentcar/BackenderentcarApplication.java
@@ -0,0 +1,13 @@
+package com.upc.pe.backenderentcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackenderentcarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackenderentcarApplication.class, args);
+    }
+
+}
