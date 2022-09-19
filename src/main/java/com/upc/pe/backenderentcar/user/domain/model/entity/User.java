4adb84c9cf72852009da27d5942f0268deb9f908
2CTO <u201521691@upc.edu.pe>
2022-09-18 20:50:05 -0500

    feat: shared resources
    shared was implemented

--- src/main/java/com/upc/pe/backenderentcar/user/domain/model/entity/User.java
+++ src/main/java/com/upc/pe/backenderentcar/user/domain/model/entity/User.java
@@ -3,6 +3,9 @@ package com.upc.pe.backenderentcar.user.domain.model.entity;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
@@ -16,4 +19,36 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 10)
+    @Column(unique = true)
+    private String name;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 10)
+    @Column(unique = true)
+    private String lastName;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 10)
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 10)
+    @Column(unique = true)
+    private String password;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 10)
+    @Column(unique = true)
+    private String imageUrl;
+
+    private int phone;
 }
