3906638782b31809db8ccc4578f114c747c56419
2CTO <u201521691@upc.edu.pe>
2022-10-01 10:51:09 -0500

    feat: updates

--- src/test/java/com/upc/pe/backenderentcar/test/step/car/US09StepDefinition.java
+++ src/test/java/com/upc/pe/backenderentcar/test/step/car/US09StepDefinition.java
@@ -4,18 +4,25 @@ import com.upc.pe.backenderentcar.shared.driver.MyWebDriver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class US09StepDefinition {
     public static WebDriver webDriver;
+
+    public US09StepDefinition() { webDriver = MyWebDriver.getWebDriver(); }
+
     @Given("User in main menu")
     public void userInMainMenu() {
-
+        webDriver.findElement(By.id("loginEmail")).sendKeys("2cto");
+        webDriver.findElement(By.id("loginPassword")).sendKeys("2cto");
+        webDriver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/form/div/button[1]")).click();
     }
 
     @When("Complete with his personal car information")
     public void completeWithHisPersonalCarInformation() {
+
     }
 
     @Then("It will complete")
--- src/test/java/com/upc/pe/backenderentcar/test/step/user/US04StepDefinition.java
+++ src/test/java/com/upc/pe/backenderentcar/test/step/user/US04StepDefinition.java
@@ -15,30 +15,29 @@ public class US04StepDefinition {
     public US04StepDefinition() { webDriver = MyWebDriver.getWebDriver(); }
     @Given("The User is in Register Section")
     public void theUserIsInRegisterSection(){
-        webDriver.get("http://127.0.0.1:5173/register");
+        webDriver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/form/div/button[2]")).click();
+
     }
 
     @And("Complete his personal information")
     public void completeHisPersonalInformation() {
-        webDriver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div/div/div/div[2]/form/div[1]/input[1]")).sendKeys("Angel");
-        webDriver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div/div/div/div[2]/form/div[1]/input[2]")).sendKeys("Herrera");
-        webDriver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div/div/div/div[2]/form/div[1]/input[3]")).sendKeys("Arrendatario");
-        webDriver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div/div/div/div[2]/form/div[1]/input[4]")).sendKeys("https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
-        webDriver.findElement(By.xpath("//*[@id=\"phone\"]")).sendKeys("978451245");
+        webDriver.findElement(By.id("registerName")).sendKeys("Romina");
+        webDriver.findElement(By.id("registerLastName")).sendKeys("Acuña");
+        webDriver.findElement(By.id("registerTypeOfUser")).sendKeys("Arrendador");
+        webDriver.findElement(By.id("registerImage")).sendKeys("image.url");
+        webDriver.findElement(By.id("registerPhone")).sendKeys("978457845");
         webDriver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div/div/div/div[2]/form/div[2]/button[2]")).click();
-        webDriver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div/div/div/div[2]/form/div[1]/input[1]")).sendKeys("angel");
-        webDriver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div/div/div/div[2]/form/div[1]/input[2]")).sendKeys("angel");
-        webDriver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div/div/div/div[2]/form/div[2]/button[2]")).click();
-
     }
 
     @When("Click the button Register")
     public void clickTheButtonRegister() {
-        webDriver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div/div[2]/button[2]")).click();
+        webDriver.findElement(By.id("registerEmail")).sendKeys("romi");
+        webDriver.findElement(By.id("registerPassword")).sendKeys("romi");
+        webDriver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div/div/div/div[2]/form/div[2]/button[2]")).click();
     }
 
     @Then("The System will show a message")
     public void theSystemWillShowAMessage() {
-        webDriver.get("http://127.0.0.1:5173/");
+        webDriver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div/div[2]/button[2]")).click();
     }
 }
